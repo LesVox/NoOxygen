22767fc46cbc63365cfb3c56286e9fc049fa71a8
Jens <joskarb@gmail.com>
2018-01-20 23:50:18 +0100

    Added player, planet and started on enemy functionality

---
+++ No Oxygen/Assets/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameController : MonoBehaviour {
+
+    public GameObject Planet;
+    public GameObject Astronaut;
+
+    private Vector3 Tilt;
+    
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+        Tilt = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
+
+        if(Astronaut.gameObject != null)
+        {
+            //Planet.transform.rotation = 
+        }
+            
+	}
+}
